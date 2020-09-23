1a0a68a98c37ff4ba611958dd0177df6d092f03e
Lorenzo Martinez <Lorenzo.s.martinez0@gmail.com>
2020-09-23 09:34:04 -0700

    Project initialization

---
+++ cmake-build-debug/CMakeFiles/GuessNum.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GuessNum.dir/main.cpp.obj"
+  "GuessNum.exe"
+  "GuessNum.exe.manifest"
+  "GuessNum.pdb"
+  "libGuessNum.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GuessNum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
