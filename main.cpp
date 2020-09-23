fdabc8ad790a7624ce3c83d4647af1f11bbf83ad
Lorenzo Martinez <Lorenzo.s.martinez0@gmail.com>
2020-09-23 10:16:15 -0700

    First working iteration of the program.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int tries = 3;
+    srand(time(0));
+    int randNumber = rand() % 10 + 1;
+    int answer;
+    bool won = false;
+
+    cout << "Hello and welcome to GUESS THAT NUMBER!" << endl << "You will have " << tries << " tries";
+    cout << " to guess the number between 1 and 10 that has been generated." << endl << endl;
+
+    for (int i = 0; i < 3; ++i) {
+        if (answer == randNumber) {
+            won = true;
+            break;
+        } else {
+            cout << "Guess number " << i + 1 << " : ";
+            cin >> answer;
+            cout << endl;
+        }
+    }
+
+    if (!won) {
+        cout << "Sorry better luck next time. The number was " << randNumber << "." << endl;
+    } else {
+        cout << "Congrats! You did it! The number was " << randNumber << "!" << endl;
+    }
+
     return 0;
 }
