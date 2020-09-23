515b605e74790b913ab06a929a45d1117bfc14d6
Lorenzo Martinez <Lorenzo.s.martinez0@gmail.com>
2020-09-23 10:29:49 -0700

    User can now play again.

--- main.cpp
+++ main.cpp
@@ -5,31 +5,41 @@
 using namespace std;
 
 int main() {
-    int tries = 3;
-    srand(time(0));
-    int randNumber = rand() % 10 + 1;
     int answer;
     bool won = false;
+    int keepGoing = 0;
 
-    cout << "Hello and welcome to GUESS THAT NUMBER!" << endl << "You will have " << tries << " tries";
-    cout << " to guess the number between 1 and 10 that has been generated." << endl << endl;
+    do {
+        srand(time(0));
+        int randNumber = rand() % 10 + 1;
 
-    for (int i = 0; i < 3; ++i) {
-        if (answer == randNumber) {
-            won = true;
-            break;
-        } else {
-            cout << "Guess number " << i + 1 << " : ";
-            cin >> answer;
-            cout << endl;
+        cout << endl << "Hello and welcome to GUESS THAT NUMBER!" << endl << "You will have 3 tries";
+        cout << " to guess the number between 1 and 10 that has been generated." << endl << endl;
+        cout << "---" << randNumber <<"---" << endl << endl;
+
+        for (int i = 0; i < 3; ++i) {
+            if (answer == randNumber) {
+                won = true;
+                break;
+            } else {
+                cout << "Guess number " << i + 1 << " : ";
+                cin >> answer;
+                cout << endl;
+            }
         }
-    }
 
-    if (!won) {
-        cout << "Sorry better luck next time. The number was " << randNumber << "." << endl;
-    } else {
-        cout << "Congrats! You did it! The number was " << randNumber << "!" << endl;
-    }
+        if (!won) {
+            cout << "Sorry better luck next time. The number was " << randNumber << "." << endl << endl;
+            cout << "Would you like to play again? [Type 1 to Play Again]" << endl;
+            cin >> keepGoing;
+        } else {
+            cout << "Congrats! You did it! The number was " << randNumber << "!" << endl << endl;
+            cout << "Would you like to play again? [Type 1 to Play Again]" << endl;
+            cin >> keepGoing;
+        }
+    } while (keepGoing == 1);
+
+    cout << "Thanks for playing the game" << endl;
 
     return 0;
 }
